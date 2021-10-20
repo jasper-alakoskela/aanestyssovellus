4b9376b5bdac74c37e2e14fb066f10bd2bfe0f53
Jasper Alakoskela <jasper.alakoskela@gmail.com>
2021-10-20 13:05:04 +0300

    korjausta

--- backend/deletePoll.php
+++ backend/deletePoll.php
@@ -6,28 +6,12 @@ if (!isset($_GET["id"])) {
 }
 
 $poll_id = $_GET["id"];
-$user_id = $_SESSION["user_id"];
+
 
 include_once "db-connection.php";
 
 try {
-    $stmt = $conn -> prepare("DELETE FROM votes WHERE user_id = :user_id AND poll_id = :poll_id;");
-    $stmt->bindParam(":user_id", $user_id);
-    $stmt->bindParam(":poll_id", $poll_id);
-
-
-    if ($stmt->execute() == false) {
-        $data = array (
-            "error" => "Virhe votes"
-        );
-    }
-
-    else {
-        $data = array (
-            "success" => "äänestys poisto onnistui"
-        );
-    }
-
+    
     $stmt = $conn -> prepare("DELETE FROM option WHERE poll_id = :poll_id;");
     $stmt->bindParam(":poll_id", $poll_id);
 
